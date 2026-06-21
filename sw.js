da16c131ac992c7aea70176baf1e320c0cf0814a
chufang1995 <168974891+chufang1995@users.noreply.github.com>
2026-06-21 09:52:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
  const CACHE = 'eng-reader-v1';
- const FILES = ['/', '/index.html', '/manifest.json', '/icons/icon.svg'];
+ const FILES = ['index.html', 'manifest.json', 'icons/icon-192.png'];
  
  self.addEventListener('install', e => {
    e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
